dc2a87cc91fa59f171542aed4b5eaaa031f7ac41
BiblicalPotato <deconinck.jordan@gmail.com>
2023-03-18 17:28:08 +0100

    Exercices 1 à 7 terminés

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < args.length; j++) {
+            if(args[j] == array[i] && typeof args[j] === typeof array[i]){
+                array.splice(i, 1);
+                j = -1;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+    if(typeof n1 != 'number' || typeof n2 != 'number'){
+        return 'ERROR'
+    }
+    else if (n1 < 0 || n2 < 0) {return 'ERROR'}
+
+    if(n1 > n2){
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+    
+    let sum = 0;
+    while(n1 <= n2){
+        sum += n1
+        n1++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
